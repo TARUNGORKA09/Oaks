0a2a2a62362fc7aef9e762ef90f95cefadd45206
Tarun Gorka <tarun.gorka@jungleworks.com>
2022-08-31 18:22:17 +0530

    payment changes

--- modules/payments/index.js
+++ modules/payments/index.js
@@ -1,4 +1,4 @@
 const razorpay = require('./razorpay')
 
 app.post("/payment/getRedirectUrl",razorpay.getRedirectUrl)
-app.post("/payment/getTransactionDetails",razorpay.getRedirectUrl)
+app.get("/payment/getTransactionDetails",razorpay.getTransactionDetails)
